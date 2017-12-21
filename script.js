9e3b90c9c8f68234b46f00b07301fe5c9ba01fe4
sarafec <fecadusa@gmail.com>
2017-12-20 21:35:39 -0800

    modify code structure

--- script.js
+++ script.js
@@ -1,74 +1,13 @@
-/* PRIORITY QUEUE IMPLEMENTATION */
-function Queue() {
-	this.dataStore = [];
-	this.enqueue = enqueue;
-	this.dequeue = dequeue;
-	this.front = front;
-	this.back = back;
-	this.toString = toString;
-	this.isEmpty = isEmpty;
-}
-
-function enqueue(elem) {
-	this.dataStore.push(elem);
-}
-
-function dequeue() {
-	let entry = 0;
-	for (let i = 0; i < this.dataStore.length; i++) {
-		if (this.dataStore[i].priority < this.dataStore[entry].code){
-			entry = i;
-		}
-	}
-	return this.dataStore.splice(entry, 1);
-}
-
-function front() {
-	return this.dataStore[0];
-}
-
-function back() {
-	return this.dataStore[this.dataStore.length - 1];
-}
-
-function toString() {
-	let formattedStr = '';
-	for (let i = 0; i < this.dataStore.length; i++) {
-		formattedStr += this.dataStore[i].priority + ' value: ' + this.dataStore[i].value + '\n';
-	}
-	return formattedStr;
-}
-
-function isEmpty() {
-	if(this.dataStore.length === 0) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-
-/* HELPER FUNCTIONS */
-function isWhiteSpace(ch) {
-	//return tab key, space, or no break space
-	return (ch === 'u0009') || (ch === '') || (ch === 'u00A0');
-}
-
-function isLetter(ch) {
-	return (ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z');
-}
-
-function isDecimalDigit(ch) {
-	return (ch >= '0') && (ch <= '9');
-}
-
-
-/* BROWSER EVENTS */
-let calculatorResult = document.querySelector('.calculator-input');
-let calcArea = document.querySelector('.grid-wrapper');
+/* APPLICATION CONSTANTS */
+const calculatorResult = document.querySelector('.calculator-input');
+const calcArea = document.querySelector('.grid-wrapper');
 
 let currentCalcVal = '';
 
+
+/* READ INPUT */
+
+/* MODIFY INPUT */
 
 function setCurrentResultVal(val) {
 	if (currentCalcVal === 0){
@@ -80,10 +19,6 @@ function setCurrentResultVal(val) {
 	}
 }
 
-function getCurrentResultVal(val) {
-	return currentCalcVal;
-}
-
 function resetCurrentResultVal(){
 	currentCalcVal = 0;
 	calculatorResult.value = 0;
@@ -93,7 +28,7 @@ function addToCalcInput(evt) {
 	if(evt.target.textContent === 'AC'){
 		return resetCurrentResultVal();
 	} else if (evt.target.textContent === '=') {
-		return readExprModule.read(calculatorResult.value);
+		return calculateVal(calculatorResult.value);
 	} else if (evt.target.className === 'operator') {
 		return setCurrentResultVal(' ' + evt.target.textContent + ' ');
 	} else {
@@ -101,7 +36,18 @@ function addToCalcInput(evt) {
 	}
 }
 
-//input events
+/* CALCULATE INPUT */
+
+function calculateVal(targetVal) {
+	let valArr = targetVal.split(' ');
+	console.log(valArr);
+}
+
+/* HELPER FUNCTIONS */
+
+
+/* BROWSER EVENTS */
+
 calcArea.addEventListener('click', function(evt){
 	if(evt.target.nodeName === "BUTTON"){
 		return addToCalcInput(evt);
@@ -121,10 +67,5 @@ calcArea.addEventListener('keydown', function(evt){
 });
 
 calculatorResult.addEventListener('change', function(evt){
-	//can we move this out to its own function?
 	currentCalcVal = evt.target.value;
 });
-
-// todo
-// 1 - add identifiers into priority queue
-// 2 - evaluate from priority queue based on precedence
